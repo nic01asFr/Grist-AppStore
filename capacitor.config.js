6adfa066401c4ab454ae36125e3b625ffb007850
Nicolas <83064733+nic01asFr@users.noreply.github.com>
2026-04-13 23:43:37 +0200

    fix: capacitor config as JS (YAML not supported by Capacitor CLI)

---
+++ capacitor.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  appId: 'fr.cerema.scoutia',
+  appName: 'SCOUT IA',
+  webDir: 'app',
+  plugins: {
+    Preferences: {
+      group: 'ScoutIAConfig'
+    }
+  },
+  android: {
+    allowMixedContent: false
+  }
+};
